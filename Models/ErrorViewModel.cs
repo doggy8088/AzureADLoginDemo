7218fca135c2a2b14db65b6523d5af8f6d48a5c3
Will <doggy.huang@gmail.com>
2022-04-10 13:40:16 +0800

    Initial commit
    dotnet new mvc -n AzureADLoginDemo --auth SingleOrg --tenant-id 76275315-xxxx-xxxx-xxxx-a329d6222150 --domain xxxxx.onmicrosoft.com --client-id 7332902c-xxxx-xxxx-xxxx-63a0dcbf3d80

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AzureADLoginDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
